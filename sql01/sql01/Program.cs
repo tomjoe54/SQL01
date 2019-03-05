c2216d35b1816d3a828f7422972eb91e12e944fc
tomjoe54 <tomjoeford@yahoo.com>
2019-03-04 19:09:07 -0800

    created project

---
+++ sql01/sql01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sql01
+{
+    class Program
+    {
+        //TODO:1 install entityframwork. add new item>data>ado model>new connection>.>select db
+
+        static void Main(string[] args)
+        {
+            ConnectToDB connection = new ConnectToDB();
+
+//create temp sorage to hold data
+            List<person> local =  connection.ImportPeople();
+
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ sql01/sql01/fvsfdsdfContextInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sql01
+{
+    //TODO:2 add the context initializer
+    class fvsfdsdfContextInitializer : DropCreateDatabaseIfModelChanges<fvsfdsdfEntities>
+    {
+        protected override void Seed(fvsfdsdfEntities context)
+        {
+
+        }
+    }
+}
